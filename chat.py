2533e24aa79811228631e59fda8adf1087fb87a1
karnansp36 <reviewmaster36@gmail.com>
2025-07-02 09:49:15 +0530

    deploy

--- chat.py
+++ chat.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 from app import create_app, socketio
+import eventlet
+eventlet.monkey_patch()
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=10000)
